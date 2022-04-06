13def29271c97d53c8d60de8a2f5c515c4d03ad5
sunyanyanlearnmse <71137806+sunyanyanlearnmse@users.noreply.github.com>
2022-04-06 08:30:29 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,12 +4,8 @@ function init(){
     var text =document.getElementById("entryinput").value;
     alert("Yanyan Sun: " + text);
     document.getElementById("textoutput").innerHTML=text；    
-    
   }
   var entryn=document.getElementById("entrybutton");
-  entryn.addEventListener("click" , clickalert);
-
+  entryn.addEventListener('click' , clickalert);
 }
-
-
 window.addEventListener('load', init);
