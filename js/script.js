9a904862463ed75f27aa086e82c72b1240ee8639
sunyanyanlearnmse <913097495@qq.com>
2022-04-06 08:00:01 +0800

    change

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function init(){
   function yysEventFunction(){
     var text1 =document.getElementById("entryinput");
     var text2 =document.getElementById("textoutput");
-    alert("Yanyan Sun:" + text1.value);
+    alert("Yanyan Sun: " + text1.value);
     text2.innerHTML=text1.value;
   }
   var entryn=document.getElementById("entrybutton");
